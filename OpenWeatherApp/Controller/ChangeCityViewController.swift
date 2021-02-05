a7fbfd1a71d58db98d093546364682496cf3b2c6
artazidele <arta.zidele@gmail.com>
2021-02-05 19:30:06 +0200

    Initial Commit

---
+++ OpenWeatherApp/Controller/ChangeCityViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ChangeCityViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Arkadijs Makarenko on 04/02/2021.
+//
+
+import UIKit
+
+protocol ChangeCityDelegate {
+    func userEnterCityName(city: String)
+}
+
+class ChangeCityViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    var delegate: ChangeCityDelegate?
+
+    @IBOutlet weak var cityTextField: DesignableTextField!
+    
+    
+    
+    @IBAction func getWeatherTapped(_ sender: Any) {
+        guard let cityName = cityTextField.text else {
+            return
+        }
+        delegate?.userEnterCityName(city: cityName)
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
